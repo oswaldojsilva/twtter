aee04542218f2a927d878c72d696c4d79afef44d
Austin Moody <amoody25@uncc.edu>
2019-04-15 16:39:18 +0000

    added username column, user profile pages, and username sign-up form.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    # Prevent CSRF attacks by raising an exception.
+    # For API's you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+    # call the configured params
+    before_action :configure_permited_parameters, if: :devise_controller?
+    
+    
+    # protect the database, while allowing these fields to be updated.
+    protected
+    def configure_permited_parameters
+        devise_parameter_sanitizer.permit(:sign_up)        { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+        devise_parameter_sanitizer.permit(:sign_in)        { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :password_confirmation) }
+    end 
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'pages#index'
   
   get 'pages/home' => 'pages#home'
-  get 'pages/profile' => 'pages#profile'
+  get '/user/:id' => 'pages#profile'
   get 'pages/explore' => 'pages#explore'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
