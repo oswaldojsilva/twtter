c01b0c01f9ae1a93f247f4caaca7e9d98dcf5597
Austin Moody <amoody25@uncc.edu>
2019-04-18 16:26:34 +0000

    added post functionality and fixed allignment issues on explore, home, and profile page

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140} #tweets can have maximum 140 chars.
   
-  default scope -> { order(created_at: :desc)} #newest tweets come first
+  default_scope { order(created_at: :desc) } #newest tweets come first
 end
